d5ad6bc00fcfc6f116d79a05b8885235173c8227
HaGiang994 <gianglehongha2805@gmail.com>
2016-02-25 10:45:27 +0700

    Add source code
    This is file ChangeLabel

---
+++ ChangeLabel/ChangeLabel/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  ChangeLabel
+//
+//  Created by MacPro1 on 1/27/16.
+//  Copyright © 2016 MacPro1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *label;
+}
+-(IBAction)changeLabel;
+
+
+@end
+
---
+++ ChangeLabel/ChangeLabel/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  ChangeLabel
+//
+//  Created by MacPro1 on 1/27/16.
+//  Copyright © 2016 MacPro1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(IBAction)changeLabel{
+    label.text = @"I have changed";
+    if (label.text == @"I have changed"){
+        label.textColor = [UIColor purpleColor];
+        
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
